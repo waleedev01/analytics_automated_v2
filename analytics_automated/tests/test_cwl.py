5b2a54bf434fd106d08936730e54693020dfada9
waleedev01 <hassanwaleedev@gmail.com>
2024-07-03 17:26:54 +0100

    simple tests

--- analytics_automated/tests/test_cwl.py
+++ analytics_automated/tests/test_cwl.py
@@ -4,11 +4,6 @@ import pytest
 from django.test import TestCase
 from analytics_automated.models import Backend, QueueType
 from analytics_automated.cwl_utils.cwl_parser import read_cwl_file
-from analytics_automated.cwl_utils.cwl_schema_validator import CWLSchemaValidator
-from analytics_automated.cwl_utils.cwl_workflow_handler import parse_cwl_workflow
-from analytics_automated.cwl_utils.cwl_clt_handler import save_task_to_db
-
-from analytics_automated.models import Job, Task, Step
 
 logger = logging.getLogger(__name__)
 
@@ -38,62 +33,4 @@ class TestCWLParser(SetupBackendQueueTestCase):
         messages = []
         task = read_cwl_file(str(cwl_file), "echo.cwl", messages)
         assert task is not None
-        assert messages == []
 
-    def test_read_cwl_file_invalid(self, tmpdir):
-        cwl_content = """
-        cwlVersion: v1.2
-        class: InvalidClass
-        baseCommand: echo
-        inputs:
-          input1:
-            type: string
-        outputs:
-          output1:
-            type: stdout
-        """
-        cwl_file = tmpdir.join("invalid.cwl")
-        cwl_file.write(cwl_content)
-
-        messages = []
-        task = read_cwl_file(str(cwl_file), "invalid.cwl", messages)
-        assert task is None
-        assert "Unknown CWL class for file" in messages[0]
-
-    def test_validate_cwl(self):
-        cwl_content = {
-            "cwlVersion": "v1.2",
-            "class": "CommandLineTool",
-            "baseCommand": "echo",
-            "inputs": {
-                "input1": {
-                    "type": "string"
-                }
-            },
-            "outputs": {
-                "output1": {
-                    "type": "stdout"
-                }
-            }
-        }
-        validator = CWLSchemaValidator()
-        is_valid, message = validator.validate_cwl(cwl_content)
-        assert is_valid
-
-    def test_parse_cwl_workflow(self):
-        cwl_data = {
-            "class": "Workflow",
-            "steps": {
-                "step1": {
-                    "run": {"class": "CommandLineTool", "baseCommand": "echo"},
-                    "in": {},
-                    "out": ["output1"]
-                }
-            }
-        }
-        messages = []
-        order_mapping = parse_cwl_workflow(cwl_data, "test_workflow", messages)
-        assert order_mapping is not None
-        assert Job.objects.filter(name="test_workflow").exists()
-        assert Task.objects.filter(name="step1").exists()
-        assert Step.objects.filter(job__name="test_workflow", task__name="step1").exists()
